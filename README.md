b2e02cf6d8fd8d7aaca9933075490d88ed2888b2
Christopher Foo <chris.foo@gmail.com>
2015-12-23 01:21:54 -0500

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# ttvsnap
-Save Twitch screenshots using Twitch API preview thumbnails
+ttvsnap
+=======
+
+Ttvsnap is script that will periodically save Twitch.tv screenshots using Twitch API preview thumbnails.
+
+Usage
+=====
+
+You will need Python 3.3+.
+
+To start the script, run something similar to this:
+
+        python3 ttvsnap.py verycoolstreamer ./screenshots/
+
+(Replace `python3` as needed, `python.exe` for example.)
+
+The script will check every ~5 minutes and save the screenshot to the given directory. For streams that run 24/7, you can use the `--subdir` option to create a directory for each day.
+
+As a convenience, it supports generating thumbnails using ImageMagick. Ensure that the `convert` command exists and add the `--thubmnail` option.
+
+For the list of options, use the `--help` option.
+
+Tips
+----
+
+If you are using this script for a website of some sort, you may want to look into some daemon service management tools to restart it if it crashes. On Linux, look into creating Upstart or Systemd configuration files for it.
+
+As of writing, Twitch caches preview images for 5 minutes so setting it to low values such as 1 minute may be useless. However, the script will use the `If-Modified-Since` HTTP header to avoid downloading images repeatedly.
+
+
---
+++ twitchscreengrab.py
@@ -0,0 +1,169 @@
+'''Save Twitch screenshots using Twitch API preview thumbnails'''
+# Copyright 2015 Christopher Foo. License: MIT.
+
+import argparse
+import datetime
+import logging
+import os
+import subprocess
+import sys
+import time
+import email.utils
+
+import requests
+import requests.exceptions
+
+
+__version__ = '1.0'
+
+_logger = logging.getLogger()
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        'channel_name', help='Username of the Twitch channel')
+    arg_parser.add_argument(
+        'output_dir', help='Path of output directory')
+    arg_parser.add_argument(
+        '--interval', type=int, default=290,
+        help='Number of seconds between each grab')
+    arg_parser.add_argument(
+        '--subdir', action='store_true',
+        help='Create subdirectories for each day.')
+    arg_parser.add_argument(
+        '--thumbnail', action='store_true',
+        help='Create thumbnails using Imagemagick "convert" command.')
+
+    args = arg_parser.parse_args()
+    logging.basicConfig(level=logging.INFO)
+
+    if not os.path.isdir(args.output_dir):
+        sys.exit('Output directory specified is not valid.')
+
+    if args.interval < 60:
+        sys.exit('Interval cannot be less than 60 seconds.')
+
+    subprocess.check_call(['convert', '-version'])
+
+    grabber = Grabber(args.channel_name, args.output_dir, args.interval,
+                      args.subdir, args.thumbnail)
+    grabber.run()
+
+
+class APIError(Exception):
+    pass
+
+
+ERROR_SLEEP_TIME = 90
+
+
+class Grabber(object):
+    def __init__(self, channel, output_dir, interval, subdir=False,
+                 thumbnail=False):
+        self._channel = channel
+        self._output_dir = output_dir
+        self._interval = interval
+        self._subdir = subdir
+        self._thumbnail = thumbnail
+        self._last_file_date = None
+
+    def run(self):
+        while True:
+            try:
+                doc = self._fetch_stream_object()
+            except requests.exceptions.RequestException:
+                _logger.exception('Error fetching stream object.')
+                time.sleep(ERROR_SLEEP_TIME)
+                continue
+
+            if 'error' in doc:
+                _logger.error('API error: %s: %s', doc['error'], doc.get('message'))
+                time.sleep(ERROR_SLEEP_TIME)
+                continue
+
+            if doc.get('stream'):
+                try:
+                    url = doc['stream']['preview']['template']
+                except KeyError:
+                    _logger.exception('Unexpected document format.')
+                    time.sleep(ERROR_SLEEP_TIME)
+                    continue
+
+                url = url.replace('{width}', '0').replace('{height}', '0')
+
+                try:
+                    path = self._fetch_image_and_save(url)
+                except (APIError, requests.exceptions.RequestException):
+                    _logger.exception('Error fetching image.')
+                    time.sleep(ERROR_SLEEP_TIME)
+                    continue
+
+                if path and self._thumbnail:
+                    self._create_thumbnail(path)
+
+            time.sleep(self._interval)
+
+    def _fetch_stream_object(self):
+        url = 'https://api.twitch.tv/kraken/streams/{}?api_version=3'.format(self._channel)
+        response = requests.get(url, timeout=60)
+        doc = response.json()
+        return doc
+
+    def _fetch_image_and_save(self, url):
+        headers = {
+            'user-agent': 'python-requests/{}'.format(requests.__version__)
+        }
+
+        if self._last_file_date:
+            headers['if-modified-since'] = self._last_file_date
+
+        response = requests.get(url, timeout=60, allow_redirects=False,
+                                headers=headers)
+
+        if response.status_code == 304:
+            return
+
+        if response.status_code != 200:
+            raise APIError('Image status code: %s', response.status_code)
+
+        self._last_file_date = response.headers['last-modified']
+
+        datetime_obj = email.utils.parsedate_to_datetime(
+            response.headers['last-modified']).astimezone(datetime.timezone.utc)
+
+        extension = url.rsplit('.', 1)[-1] or 'bin'
+
+        path_parts = [self._output_dir]
+
+        if self._subdir:
+            path_parts.append(datetime_obj.strftime('%Y-%m-%d'))
+            subdir_path = os.path.join(*path_parts)
+
+            if not os.path.exists(subdir_path):
+                os.mkdir(subdir_path)
+
+        path_parts.append('{}.{}'.format(
+            datetime_obj.strftime('%Y-%m-%d_%H-%M-%S'),
+            extension
+        ))
+
+        path = os.path.join(*path_parts)
+
+        with open(path, 'wb') as file:
+            for part in response.iter_content():
+                file.write(part)
+
+        _logger.info('Saved %s', path)
+
+        return path
+
+    def _create_thumbnail(self, path):
+        basename, ext = os.path.splitext(path)
+        thumbnail_path = '{}_thumb{}'.format(basename, ext)
+
+        subprocess.check_call(['convert', path, '-thumbnail', 'x144',
+                               thumbnail_path])
+
+
+if __name__ == '__main__':
+    main()
